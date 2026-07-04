483cf40eef9bf1b71b7b0d73122e1064411e9aee
tanisha0519 <tanishas1910@gmail.com>
2026-07-04 17:07:10 +0530

    Add files via upload

---
+++ q-34.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() {
+    int num, i, isPrime = 1;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    if (num <= 1) {
+        isPrime = 0;
+    } else {
+        for (i = 2; i <= num / 2; i++) {
+            if (num % i == 0) {
+                isPrime = 0;
+                break;
+            }
+        }
+    }
+    if (isPrime)
+        printf("%d is a Prime number.\n", num);
+    else
+        printf("%d is not a Prime number.\n", num);
+    return 0;
+}
