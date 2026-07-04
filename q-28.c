158f8a03e541f00574386280017f49d8556b988d
tanisha0519 <tanishas1910@gmail.com>
2026-07-04 17:05:02 +0530

    Add files via upload

---
+++ q-28.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+    int n, i;
+    long long factorial = 1;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    if (n < 0) {
+        printf("Factorial of a negative number does not exist.\n");
+    } else {
+        for (i = 1; i <= n; i++) {
+            factorial *= i;
+        }
+        printf("Factorial of %d = %lld\n", n, factorial);
+    }
+    return 0;
+}
