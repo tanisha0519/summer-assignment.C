19c7490a0e40c761f0023887405089336d624d5f
tanisha0519 <tanishas1910@gmail.com>
2026-07-04 17:26:37 +0530

    Add files via upload

---
+++ q-50.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    int x, n, i;
+    long long result = 1;
+    printf("Enter the base (x): ");
+    scanf("%d", &x);
+    printf("Enter the exponent (n): ");
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++) {
+        result = result * x;
+    }
+    printf("%d^%d = %lld\n", x, n, result);
+    return 0;
+}
