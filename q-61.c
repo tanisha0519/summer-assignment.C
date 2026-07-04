dbd0acdc8a0fc162d03f859ce291754e7169bd3f
tanisha0519 <tanishas1910@gmail.com>
2026-07-04 17:30:26 +0530

    Add files via upload

---
+++ q-61.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    int first = 0, second = 1, next;
+
+    printf("Enter the number of terms: ");
+    scanf("%d", &n);
+
+    printf("Fibonacci Series: ");
+
+    for(i = 1; i <= n; i++) {
+        printf("%d ", first);
+        next = first + second;
+        first = second;
+        second = next;
+    }
+
+    return 0;
+}
