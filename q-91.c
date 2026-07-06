ede74bb5c501bacd5b2169a09649509d59d91552
tanisha0519 <tanishas1910@gmail.com>
2026-07-06 13:53:36 +0530

    Add files via upload

---
+++ q-91.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+    int arr[100], n, i, key, count = 0;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    printf("Enter the element to find frequency: ");
+    scanf("%d", &key);
+    for (i = 0; i < n; i++) {
+        if (arr[i] == key) {
+            count++;
+        }
+    }
+    printf("Frequency of %d = %d\n", key, count);
+    return 0;
+}
