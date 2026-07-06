6c9af9c4c48f2e50d859fe8dd6903d29252e609e
tanisha0519 <tanishas1910@gmail.com>
2026-07-06 13:51:20 +0530

    Add files via upload

---
+++ q-85.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int isPerfect(int n) {
+    int i, sum = 0;
+    for (i = 1; i <= n / 2; i++) {
+        if (n % i == 0)
+            sum += i;
+    }
+    return (sum == n);
+}
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    if (isPerfect(num))
+        printf("%d is a Perfect Number.\n", num);
+    else
+        printf("%d is not a Perfect Number.\n", num);
+    return 0;
+}
