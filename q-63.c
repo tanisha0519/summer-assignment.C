5dfbc5ec386dcf7d75c5ecec1f3833046fd315ab
tanisha0519 <tanishas1910@gmail.com>
2026-07-06 13:38:17 +0530

    Add files via upload

---
+++ q-63.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int sumOfDigits(int n) {
+    if (n == 0)
+        return 0;
+    else
+        return (n % 10) + sumOfDigits(n / 10);
+}
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    printf("Sum of digits = %d\n", sumOfDigits(num));
+    return 0;
+}
