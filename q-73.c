6fd21f662e34c6a0f61369b33316466f8c317069
tanisha0519 <tanishas1910@gmail.com>
2026-07-06 13:44:43 +0530

    Add files via upload

---
+++ q-73.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+    int i, j, rows;
+    printf("Enter the size of the square: ");
+    scanf("%d", &rows);
+    for (i = 1; i <= rows; i++) {
+        for (j = 1; j <= rows; j++) {
+            if (i == 1 || i == rows || j == 1 || j == rows)
+                printf("*");
+            else
+                printf(" ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
