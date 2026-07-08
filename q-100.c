45e6a464b7aa2d752354b2e24390b85d50cf72c5
tanisha0519 <tanishas1910@gmail.com>
2026-07-09 00:14:14 +0530

    Update q-100.c

--- q-100.c
+++ q-100.c
@@ -3,7 +3,7 @@ int main() {
     int arr[100], n, i, j, sum;
     int found = 0;
     printf("Enter the number of elements: ");
-    scanf("%d", &n);
+    scanf("%d", &n); 
     printf("Enter %d elements:\n", n);
     for (i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
